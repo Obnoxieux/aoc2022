a121671e30643e7676f3df125136670aab0c512e
David Battefeld <davidbattefeld@gmail.com>
2022-12-04 10:35:54 +0100

    day 2 part 2 working

--- src/Day2.kt
+++ src/Day2.kt
@@ -3,31 +3,37 @@ package src
 import java.io.File
 
 //--- Day 2: Rock Paper Scissors ---//
+//Part 2 is really tasty spaghetti, mmmhhhh
 
 enum class GameResult {
-    WON, LOST, DRAW
+    WON, LOST, DRAWN
+}
+
+enum class PlayerGoal {
+    WIN, LOSE, DRAW
 }
 
 fun getRPSGameScore(opponent: String, player: String): Int {
     var score = 0
 
-    when (player) {
-        "X" -> score += 1
-        "Y" -> score += 2
-        "Z" -> score += 3
+    val replacedOpponent = opponent.replace("A", "Rock").replace("B", "Paper").replace("C", "Scissors")
+    val replacedPlayer = player.replace("X", "Rock").replace("Y", "Paper").replace("Z", "Scissors")
+
+    when (replacedPlayer) {
+        "Rock" -> score += 1
+        "Paper" -> score += 2
+        "Scissors" -> score += 3
         else -> {
             print("wrong values here, please disregard result")
         }
     }
-    val replacedOpponent = opponent.replace("A", "Rock").replace("B", "Paper").replace("C", "Scissors")
-    val replacedPlayer = player.replace("X", "Rock").replace("Y", "Paper").replace("Z", "Scissors")
 
     val result = determineGameResult(replacedOpponent, replacedPlayer)
 
     score += when (result) {
         GameResult.WON -> 6
         GameResult.LOST -> 0
-        GameResult.DRAW -> 3
+        GameResult.DRAWN -> 3
     }
     //print("\nTotal Score added for this round is $score, result was $result")
     return score
@@ -35,7 +41,7 @@ fun getRPSGameScore(opponent: String, player: String): Int {
 
 fun determineGameResult(opponent: String, player: String): GameResult {
     return if (player == opponent) {
-        GameResult.DRAW
+        GameResult.DRAWN
     } else if (player == "Rock" && opponent == "Scissors" || player == "Scissors" && opponent == "Paper" || player == "Paper" && opponent == "Rock") {
         GameResult.WON
     } else {
@@ -43,19 +49,80 @@ fun determineGameResult(opponent: String, player: String): GameResult {
     }
 }
 
+fun determinePlayerGoal(codeword: String): PlayerGoal {
+    return if (codeword == "X") {
+        PlayerGoal.LOSE
+    } else if (codeword == "Y") {
+        PlayerGoal.DRAW
+    } else {
+        PlayerGoal.WIN
+    }
+}
+
+fun chooseCorrectShape(goal: PlayerGoal, opponentsChoice: String): String {
+    val replacedOpponent = opponentsChoice.replace("A", "Rock").replace("B", "Paper").replace("C", "Scissors")
+
+    //trying to return the semantic values directly here, let's see if the replace func later does not fuck this up
+    when (goal) {
+        PlayerGoal.WIN -> {
+            return when (replacedOpponent) {
+                "Rock" -> "Paper"
+                "Paper" -> "Scissors"
+                "Scissors" -> "Rock"
+                else -> {
+                    "this is not a valid value"
+                }
+            }
+        }
+        PlayerGoal.LOSE -> {
+            return when (replacedOpponent) {
+                "Rock" -> "Scissors"
+                "Paper" -> "Rock"
+                "Scissors" -> "Paper"
+                else -> {
+                    "this is not a valid value"
+                }
+            }
+        }
+        PlayerGoal.DRAW -> {
+            return when (replacedOpponent) {
+                "Rock" -> "Rock"
+                "Paper" -> "Paper"
+                "Scissors" -> "Scissors"
+                else -> {
+                    "this is not a valid value"
+                }
+            }
+        }
+    }
+}
+
+fun calculatePart1(games: Array<String>) {
+    var totalScore = 0
+    for (game in games) {
+        val opponentsChoice = game.first().toString()
+        val playersChoice = game.last().toString()
+        totalScore += getRPSGameScore(opponentsChoice, playersChoice)
+    }
+    print("Total Score for Part 1 is $totalScore")
+}
+
+fun calculatePart2(games: Array<String>) {
+    var totalScore = 0
+    for (game in games) {
+        val opponentsChoice = game.first().toString()
+        val playersCodeword = game.last().toString()
+        val goal = determinePlayerGoal(codeword = playersCodeword)
+        val playersChoice = chooseCorrectShape(goal = goal, opponentsChoice = opponentsChoice)
+        totalScore += getRPSGameScore(opponentsChoice, playersChoice)
+    }
+    print("\nTotal Score for Part 2 according to strategy is $totalScore")
+}
+
 fun calculateDay2() {
     val filename = "inputs/day2.txt"
     val inputContent = File(filename).readText()
     val games = inputContent.split("\n").toTypedArray()
-    //println(games.contentToString())
-
-    var totalScore = 0
-    for (game in games) {
-        val playersChoice = game.last().toString()
-        val opponentsChoice = game.first().toString()
-        totalScore += getRPSGameScore(opponentsChoice, playersChoice)
-        //DEBUG
-        //print("\n$totalScore")
-    }
-    print(totalScore)
+    calculatePart1(games = games)
+    calculatePart2(games = games)
 }
